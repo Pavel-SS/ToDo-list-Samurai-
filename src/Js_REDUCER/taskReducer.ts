9eb75878bdde611cebe7b71b6beab6582de4e9b0
Pavel <sosnovskiy.pavel@inbox.ru>
2021-12-23 20:19:47 +0300

    Update taskReducer.ts

--- src/Js_REDUCER/taskReducer.ts
+++ src/Js_REDUCER/taskReducer.ts
@@ -13,3 +13,27 @@ export function mult(state:number, num:number){
 export function divider(state:number, num:number){
     return state / num
 }
+
+//1. Что на старте // 570$
+//2. Тип действия // 'sum'
+//3. Доп параметры(коэффициенты или надбавки к ЗП)// 330$
+
+//Создадим некий объект который будет описывать действия
+export type ActionType = {
+    type:'sum'| 'sub'| 'mult' | 'divider',
+    payload: number
+}
+
+export const actionCreator = (type:string, payload:number)=> ({
+    type,
+    payload
+})
+
+export const salaryReducer = (state: number, action: ActionType): number => {
+    switch(action.type){
+        case 'sum':
+            return state + action.payload
+        default:
+            return state
+    }
+}
