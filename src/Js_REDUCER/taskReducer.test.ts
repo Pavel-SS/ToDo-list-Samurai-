071bfe347af5291a77bdf8d18dd2eae2572c8fdf
Pavel <sosnovskiy.pavel@inbox.ru>
2021-12-23 20:47:37 +0300

    Update taskReducer.test.ts

--- src/Js_REDUCER/taskReducer.test.ts
+++ src/Js_REDUCER/taskReducer.test.ts
@@ -1,4 +1,4 @@
-import {sum,sub,mult,divider, salaryReducer} from './taskReducer';
+import {sum,sub,mult,divider, salaryReducer,multAC} from './taskReducer';
 test('sum', ()=>{
     //1. тестовые данные
     const a: number = 570,
@@ -37,10 +37,35 @@ test('divider', ()=>{
 
 })
 
-test('reducer', ()=>{
+test('reducerSum', ()=>{
     //1. тестовые данные
     //2. выполнение тестируемого кода
     //3. проверка ожидаемого результата
     expect(salaryReducer(570,{type:'sum', payload: 330})).toBe(900);
+    expect(salaryReducer(570,{type:'sub', payload: 330})).toBe(240);
+    expect(salaryReducer(70,{type:'mult', payload: 30})).toBe(2100);
+    expect(salaryReducer(500,{type:'divider', payload: 2})).toBe(250);
+
+})
+test('reducerSub', ()=>{
+    //1. тестовые данные
+    //2. выполнение тестируемого кода
+    //3. проверка ожидаемого результата
+    expect(salaryReducer(570,{type:'sub', payload: 330})).toBe(240);
+
+})
+test('reducerMult', ()=>{
+    //1. тестовые данные
+    //2. выполнение тестируемого кода
+    //3. проверка ожидаемого результата
+    const actionMult =multAC('mult', 30);
+    expect(salaryReducer(70, actionMult)).toBe(2100);
+
+})
+test('reducerDivider', ()=>{
+    //1. тестовые данные
+    //2. выполнение тестируемого кода
+    //3. проверка ожидаемого результата
+    expect(salaryReducer(500,{type:'divider', payload: 2})).toBe(250);
 
 })
