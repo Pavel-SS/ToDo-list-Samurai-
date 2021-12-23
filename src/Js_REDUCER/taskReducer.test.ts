46f8a85d2d9dc7680c408d267e4fd824d1766177
Pavel <sosnovskiy.pavel@inbox.ru>
2021-12-23 19:56:57 +0300

    Create taskReducer.test.ts

---
+++ src/Js_REDUCER/taskReducer.test.ts
@@ -0,0 +1,38 @@
+import {sum,sub,mult,divider} from './taskReducer';
+test('sum', ()=>{
+    //1. тестовые данные
+    const a: number = 570,
+        b: number = 330,
+    //2. выполнение тестируемого кода
+        result = sum(a,b)
+    //3. проверка ожидаемого результата
+    expect(result).toBe(900);
+
+})
+test('sub', ()=>{
+    //1. тестовые данные
+    const a: number = 570,
+        b: number = 330,
+    //2. выполнение тестируемого кода
+        result = sub(a,b)
+    //3. проверка ожидаемого результата
+    expect(result).toBe(240);
+
+})
+test('mult', ()=>{
+    //1. тестовые данные
+    //2. выполнение тестируемого кода
+    //3. проверка ожидаемого результата
+    expect(mult(2,3)).toBe(6);
+
+})
+test('divider', ()=>{
+    //1. тестовые данные
+    const a: number = 10,
+        b: number = 5,
+    //2. выполнение тестируемого кода
+        result = divider(a,b)
+    //3. проверка ожидаемого результата
+    expect(result).toBe(2);
+
+})
