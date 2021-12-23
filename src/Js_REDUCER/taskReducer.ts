78ab3ac428043fd2b2f791f33b620cf47c8d81d5
Pavel <sosnovskiy.pavel@inbox.ru>
2021-12-23 19:56:53 +0300

    Create taskReducer.ts

---
+++ src/Js_REDUCER/taskReducer.ts
@@ -0,0 +1,15 @@
+export function sum(state:number, num:number){
+    return state + num
+}
+
+export function sub(state:number, num:number){
+    return state - num
+}
+
+
+export function mult(state:number, num:number){
+    return state * num
+}
+export function divider(state:number, num:number){
+    return state / num
+}
