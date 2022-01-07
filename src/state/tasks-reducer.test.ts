164a088557c57dc21bebb8199cb9bf555d920dec
Pavel <sosnovskiy.pavel@inbox.ru>
2022-01-07 22:39:00 +0300

    Update tasks-reducer.test.ts

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -1,5 +1,5 @@
 import {tasksReducer, removeTaskAC, addTaskAC, changeTaskStatusAC, changeTaskTitleAC } from './tasks-reducer';
-import {addTodoListAC} from './todolist-reducer'
+import {addTodoListAC, removeTodoListAC} from './todolist-reducer'
 import {TaskStateType} from '../App';
 
 test('correct task should be deleted from correct array', () => {
@@ -146,3 +146,27 @@ test('new array should be added whrn new todolist is added', () => {
     expect (keys.length).toBe(3);
     expect (endState[newKey]).toStrictEqual([]);
 });
+
+test('property with todolistId should be deleted', () => {
+    const startState: TaskStateType = {
+        "todolistId1": [
+            { id: "1", title: "CSS", isDone: false },
+            { id: "2", title: "JS", isDone: true },
+            { id: "3", title: "React", isDone: false }
+        ],
+        "todolistId2": [
+            { id: "1", title: "bread", isDone: false },
+            { id: "2", title: "milk", isDone: true },
+            { id: "3", title: "tea", isDone: false }
+        ]
+    };
+
+    const action = removeTodoListAC('todolistId2');
+
+    const endState = tasksReducer(startState, action)
+
+    const keys = Object.keys(endState);
+
+    expect(keys.length).toBe(1);
+    expect(endState["todolistId2"]).toBeUndefined()
+})
