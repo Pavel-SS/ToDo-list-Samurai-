c6d2a8a4ebb9763d181d69639789d7a9a6a49be0
Pavel <sosnovskiy.pavel@inbox.ru>
2022-01-27 20:24:39 +0300

    Update jest.config.js

--- integration/jest.config.js
+++ integration/jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     preset: 'jest-puppeteer',
     testRegex: './*\\.test\\.js$',
-    setupFilesAfterEnv: ['./setupTests.js']
+    setupFilesAfterEnv: ['./setupTests.js'],
+    testTimeout: 50000
  };
  
  
