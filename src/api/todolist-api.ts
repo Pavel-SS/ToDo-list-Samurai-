359c045d6bb237261a336d7cae0f72680679bcd9
Pavel <sosnovskiy.pavel@inbox.ru>
2022-02-05 20:28:16 +0300

    Update todolist-api.ts

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -8,17 +8,38 @@ const instance = axios.create({
         'API-KEY': '9ae64b0c-5b04-4282-b441-a5689edb342c'
     }
 })
+type GetTodoType = {
+    addedDate: string
+    id: string
+    order: number
+    title: string
+}
+type CreateTodoType = {
+    resultCode: number
+    messages: Array<string>
+    data: {
+      item: GetTodoType
+    }
+    fieldsErrors: Array<string>
+}
+
+type DelUpdateTodoType = {
+    resultCode: number
+    messages: Array<string>
+    data: {}
+    fieldsErrors: Array<string>
+}
 export const todolistAPI = {
-    getTodos(){
-       return instance.get('todo-lists')
+    getTodos: () => {
+       return instance.get<GetTodoType[]>('todo-lists')
     },
-    createTodos(title: string){
-        return instance.post('todo-lists', {title})
+    createTodos: (title: string) => {
+        return instance.post<CreateTodoType>('todo-lists', {title})
     },
-    deleteTodos(todolistId: string){
-        return instance.delete(`todo-lists/${todolistId}`)
+    deleteTodos: (todolistId: string) => {
+        return instance.delete<DelUpdateTodoType>(`todo-lists/${todolistId}`)
     },
-    updateTodos(todolistId: string, title: string){
-        return instance.put(`todo-lists/${todolistId}`, {title} )
+    updateTodos: (todolistId: string, title: string) => {
+        return instance.put<DelUpdateTodoType>(`todo-lists/${todolistId}`, {title} )
     }
 }
