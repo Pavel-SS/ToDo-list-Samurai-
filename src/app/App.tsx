152f55eef3951e0e129abe5e471c4503e132fc2d
Pavel <sosnovskiy.pavel@inbox.ru>
2022-02-16 23:28:23 +0300

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './App.css'
 import { TodolistsList } from '../features/TodolistsList/TodolistsList'
+import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnakbar';
+import { useAppSelector } from './store';
+// import { useSelector } from 'react-redux';
+import { RequestStatusType } from './app-reducer';
 
 // You can learn about the difference by reading this guide on minimizing bundle size.
 // https://mui.com/guides/minimizing-bundle-size/
@@ -11,16 +15,25 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-import { Menu } from '@mui/icons-material';
-import { LinearProgress } from '@mui/material';
-import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnakbar';
+import Menu from '@mui/icons-material/Menu';
+import LinearProgress from '@mui/material/LinearProgress';
+
 
 
 function App() {
+//useSelector хук принимает selector функцию для выбора данных из хранилища и другой функции , equalityFnчтобы сравнить их , прежде чем вернуться результатами и определить , когда , чтобы сделать , если данные из предыдущего и текущего состояния различны.
+    //useSelector всегда принимает стэйт всего приложения, а мы потом уже достаем то что нам нужно
+    //Типизация: первым параметром мы пишем типизацию стейта всего приложения, а вторым мы указываем возвращаемый тип
+
+    // Один из вариантов для передачи 
+    // const state = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
+
+    // Второй вариант более практичный
+    //useAppSelector в сторе
+ const state = useAppSelector<RequestStatusType>((state) => state.app.status)
 
     return (
-        <div className="App">
-            <ErrorSnackbar/>
+        <div className="App">   
             <AppBar position="static">
                 <Toolbar>
                     <IconButton edge="start" color="inherit" aria-label="menu">
@@ -31,11 +44,12 @@ function App() {
                     </Typography>
                     <Button color="inherit">Login</Button>
                 </Toolbar>
-                <LinearProgress/>
             </AppBar>
+                {state === 'loading' && <LinearProgress color="secondary"/>}
             <Container fixed>
                 <TodolistsList/>
             </Container>
+            <ErrorSnackbar/>
         </div>
     )
 }
