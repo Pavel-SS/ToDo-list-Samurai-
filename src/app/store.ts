e89569577ec2c76bb16c5b5e0829f793d61275f9
Pavel <sosnovskiy.pavel@inbox.ru>
2022-02-16 23:28:26 +0300

    Update store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,6 +3,7 @@ import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
 import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
 import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -16,6 +17,9 @@ export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
+//useAppSelector - селектор всего приложения
+//
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
