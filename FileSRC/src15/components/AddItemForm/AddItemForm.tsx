42235091b966ecc242868ee1ee739664dc7d7281
Pavel <sosnovskiy.pavel@inbox.ru>
2022-02-23 21:51:59 +0300

    init: ls16

--- FileSRC/src15/components/AddItemForm/AddItemForm.tsx
+++ FileSRC/src15/components/AddItemForm/AddItemForm.tsx
@@ -2,21 +2,22 @@ import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import { AddBox } from '@mui/icons-material';
+import { RequestStatusType } from '../../app/app-reducer';
 
 type AddItemFormPropsType = {
     addItem: (title: string) => void
     disabled?: boolean
 }
 
-export const AddItemForm = React.memo(function ({addItem, disabled = false}: AddItemFormPropsType) {
+export const AddItemForm = React.memo(function (props: AddItemFormPropsType) {
     console.log('AddItemForm called')
 
     let [title, setTitle] = useState('')
     let [error, setError] = useState<string | null>(null)
 
-    const addItemHandler = () => {
+    const addItem = () => {
         if (title.trim() !== '') {
-            addItem(title);
+            props.addItem(title);
             setTitle('');
         } else {
             setError('Title is required');
@@ -32,21 +33,21 @@ export const AddItemForm = React.memo(function ({addItem, disabled = false}: Add
             setError(null);
         }
         if (e.charCode === 13) {
-            addItemHandler();
+            addItem();
         }
     }
 
     return <div>
         <TextField variant="outlined"
-                   disabled={disabled}
                    error={!!error}
                    value={title}
                    onChange={onChangeHandler}
                    onKeyPress={onKeyPressHandler}
                    label="Title"
                    helperText={error}
+                   disabled={props.disabled}
         />
-        <IconButton color="primary" onClick={addItemHandler} disabled={disabled}>
+        <IconButton color="primary" onClick={addItem} disabled={props.disabled}>
             <AddBox/>
         </IconButton>
     </div>
--- FileSRC/src15v2/app/store.ts
+++ FileSRC/src15v2/app/store.ts
@@ -1,8 +1,10 @@
-import { tasksReducer } from '../features/TodolistsList/tasks-reducer';
-import { todolistsReducer } from '../features/TodolistsList/todolists-reducer';
-import { applyMiddleware, combineReducers, createStore } from 'redux'
+import { useSelector, TypedUseSelectorHook } from 'react-redux';
+
+import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
+import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
+import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import { appReducer } from './app-reducer'
+import { appReducer } from './app-reducer';
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -15,7 +17,7 @@ const rootReducer = combineReducers({
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
-
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector;
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
