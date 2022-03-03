05639bfa2f69dc3669a15ba822b3228edbfaac23
Pavel <sosnovskiy.pavel@inbox.ru>
2022-03-03 22:07:19 +0300

    Update todolist-api.ts

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,11 +1,5 @@
 import axios from 'axios'
 
-const settings = {
-    withCredentials: true,
-    headers: {
-       'API-KEY': '4d0524f8-ac75-4061-8829-77c9b9ae1d87'
-   }
- }
  const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
@@ -13,17 +7,34 @@ const settings = {
         'API-KEY': '4d0524f8-ac75-4061-8829-77c9b9ae1d87'
     }
 })
+
+
+
 export const todolistAPI = {
    getTodolist() {
-    return instance.get('todo-lists')
+    return instance.get<TodolistType[]>('todo-lists')
    },
    createTodolist(title:string){
-    return instance.post('todo-lists', {title})
+    return instance.post<ResponseType[]>('todo-lists', {title})
    },
    deleteTodolist(todolistId:string){
-    return instance.delete( `todo-lists/${todolistId}`)
+    return instance.delete<ResponseType[]>( `todo-lists/${todolistId}`)
    },
    updateTodolistTitle(todolistId:string, title:string){
-    return instance.put(`todo-lists/${todolistId}`, {title})
+    return instance.put<ResponseType[]>(`todo-lists/${todolistId}`, {title})
    }
+}
+
+export type TodolistType = {
+    id: string
+    title: string
+    addedDate: string
+    order: number
+}
+
+export type ResponseType<D = {}> = {
+    resultCode: number
+    messages: Array<string>
+    fieldsErrors: Array<string>
+    data: D
 }
